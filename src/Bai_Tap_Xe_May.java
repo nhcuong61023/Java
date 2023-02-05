d4589b6a9e521876459e17e6e0f5f34e89b87929
nhcuong61023 <yuneroxtom@gmail.com>
2023-02-05 23:41:25 +0700

    add files

--- src/Bai_Tap_Xe_May.java
+++ src/Bai_Tap_Xe_May.java
@@ -1,11 +1,11 @@
-public class Xe_May {
+class Xe_May {
     String hang;
     String model;
     Float gia;
     String dung_Tich;
     String chu_Xe;
     Integer so_Km_da_chay;
-
+    public Xe_May(){}
     public Xe_May(String hang, String model, Float gia, String dung_Tich, String chu_Xe, Integer so_Km_da_chay) {
         this.hang = hang;
         this.model = model;
@@ -13,29 +13,28 @@ public class Xe_May {
         this.dung_Tich = dung_Tich;
         this.chu_Xe = chu_Xe;
         this.so_Km_da_chay = so_Km_da_chay;
-    }
-
-    public Xe_May() {
-    }
-
+    }   
     public void danh_gia() {
         if (this.so_Km_da_chay > 20000) {
             System.out.println("Xe cũ");
-        } else if (this.so_Km_da_chay < 600) {
-            System.out.println("Xe vẫn còn mới");
+        } else if (this.so_Km_da_chay < 300) {
+            System.out.println("Xe rất mới");
+        }else
+        {
+            System.out.println("Xe còn khá mới");
         }
     }
-
     public void thong_tin() {
         System.out.println("Chủ xe là: " + this.chu_Xe);
         System.out.println("Hãng xe là: " + this.hang);
         System.out.println("Model: " + this.model);
         System.out.println("Giá: " + this.gia);
         System.out.println("Dung tích của xe: " + this.dung_Tich);
-    }
-
+    } 
+}
+public class Bai_Tap_Xe_May{
     public static void main(String[] args) {
-        Xe_May xe1 = new Xe_May("HonDa", "54RTX", 120000.0f, "120cc", "Anh Cuong_Pro", 500);
+        Xe_May xe1 = new Xe_May("HonDa", "54RTX", 120000.0f, "120cc", "Cuong", 500);
         xe1.thong_tin();
         xe1.danh_gia();
     }
---
+++ src/test.java
@@ -0,0 +1,22 @@
+class demo{
+    int balance = 0;
+    int id;
+    String name;
+    public demo(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+    public void deposit(int amount){
+        System.out.println("So Tiền Được gửi vào là: "+ amount);
+    }
+    public void withdraw(int amount) {
+        System.out.println("Số tiền rút là: " + amount);
+    }
+    public void display(){
+        System.out.println("Số tiền sau khi thực hiện giao dịch là: " );
+    }
+}
+public class test {
+    demo acc1 = new demo(12312, "Cuong_Pro");
+    a
+}
