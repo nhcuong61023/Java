328037d749555ef256b233b393f2a6a4573d9ae1
nhcuong61023 <yuneroxtom@gmail.com>
2023-02-12 22:29:39 +0700

    add files

---
+++ src/Class_Person/Entry.java
@@ -0,0 +1,9 @@
+public class Entry {
+	public static void main(String[] args) {
+		Student s = new Student("Cuong", 23, "Ba Don", 4.0);
+		s.display();
+		System.out.println();
+		Teacher t = new Teacher("Van", 45, "Dong Hoi", 999999999);
+		t.display();
+	}
+}
